61d6462d10dde494afa70d5d2a7096bb79641ee8
Amjad-Lekhdar <amjadlekhdar@gmail.com>
2020-02-18 12:06:23 -0500

    Modification de l'emplacement de la partie 2 du tp1

---
+++ tp1/src/tp1Partie2/Client.java
@@ -0,0 +1,24 @@
+package tp1Partie2;
+
+public class Client {
+	
+	
+	
+	static String nom;
+	static String plat;
+	static int nbCommande;
+	static double total;
+	
+	public Client(String pNom, String pPlat, int pNbCommande, double pTotal) {
+		
+		nom=pNom;
+		plat=pPlat;
+		nbCommande=pNbCommande;
+		total=pTotal;
+				
+		
+		
+	}
+	
+	
+}
